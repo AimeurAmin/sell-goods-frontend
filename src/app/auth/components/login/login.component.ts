cb4d8997a8c2e879c568149901c09b6c7333ffb6
Amin AIMEUR <m.amin.aimeur@gmail.com>
2021-09-13 13:58:41 +0100

    login page design

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  hidePassword = true
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  togglePassword() {
+    this.hidePassword = !this.hidePassword;
   }
 
 }
